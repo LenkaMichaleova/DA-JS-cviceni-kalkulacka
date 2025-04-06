eb76b588da64c7a8b30e001a18ace8f5ad6f34ce
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-04-06 23:17:58 +0200

    vyřešeno i s bonusem

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const displayElm = document.querySelector(".display")
 
+const handleDigitClick = (event) => {
+    // console.log("Uzivatel klik na tlacitko")
+    // console.log(event.target)
+    const digit = event.target.textContent
+    // console.log(digit)
+    if (displayElm.textContent === "0") {
+        displayElm.textContent = digit
+    } else if (displayElm.textContent.length < 9) {
+        displayElm.textContent += digit
+    }
+}
+
+displayElm.textContent = 0 // 42
+
+document.querySelector("#btn-0").addEventListener("click", handleDigitClick)
+document.querySelector("#btn-1").addEventListener("click", handleDigitClick)
+document.querySelector("#btn-2").addEventListener("click", handleDigitClick)
+document.querySelector("#btn-3").addEventListener("click", handleDigitClick)
+document.querySelector("#btn-4").addEventListener("click", handleDigitClick)
+document.querySelector("#btn-5").addEventListener("click", handleDigitClick)
+document.querySelector("#btn-6").addEventListener("click", handleDigitClick)
+document.querySelector("#btn-7").addEventListener("click", handleDigitClick)
+document.querySelector("#btn-8").addEventListener("click", handleDigitClick)
+document.querySelector("#btn-9").addEventListener("click", handleDigitClick)
